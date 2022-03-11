595a5f726c86cd3cf5b88f7b8a73cf7850bb1a0b
Giovanna Linda <eugiovannasouza@gmail.com>
2022-03-11 00:24:10 -0300

    creating the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { Home } from 'pages'
+import { HOME } from 'routes'
+
 function App() {
-  return <h1>Hello World! :D</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={HOME} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,3 @@
+export function Home() {
+  return <h1>Hello</h1>
+}
