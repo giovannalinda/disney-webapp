04d81e922e25be0a05ac02368f36cbdad63d5809
Giovanna Linda <eugiovannasouza@gmail.com>
2022-03-15 00:46:13 -0300

    adding themes for dark and light mode

---
+++ src/config/themes/dark.ts
@@ -0,0 +1,5 @@
+export default {
+  backgroundColor: '#1A1D29',
+  textColor: '#F9F9F9',
+  headerBackgroundColor: '#090B13',
+}
---
+++ src/config/themes/light.ts
@@ -0,0 +1,5 @@
+export default {
+  backgroundColor: '#f2f2f2',
+  textColor: '#090B13',
+  headerBackgroundColor: '#F9F9F9',
+}
